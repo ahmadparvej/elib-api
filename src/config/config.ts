7aa7613e7cb1b5b2f51da7b20580e11938b336bc
ahmadparvej <ahmadparvej58@gmail.com>
2024-09-23 16:15:02 +0530

    add mongodb connection

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,7 +2,8 @@ import { config as conf } from 'dotenv'
 conf();
 
 const _config = {
-    port: process.env.PORT
+    PORT: process.env.PORT,
+    MONGO_URI: process.env.MONGO_URI
 }
 
 export const config = Object.freeze(_config);
