24a0a6ef05f3b101f0dec53c4ba109267c37419b
ahmadparvej <ahmadparvej58@gmail.com>
2024-09-25 08:34:23 +0530

    install bcrypt, password hash

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -2,6 +2,7 @@
 import { Request } from 'express';
 import { Response } from 'express';
 import { NextFunction } from 'express';
+import bcrypt from 'bcrypt';
 import createHttpError from 'http-errors';
 import userModel from './userModel';
 
@@ -21,6 +22,10 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
         const error = createHttpError(400, "User already exist")
         return next(error)
     }
+
+    //Password validation
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
     //process
 
     //response
