2013367892910fe2b4e06a8d64e95d48a7875304
ahmadparvej <ahmadparvej58@gmail.com>
2024-09-25 08:26:49 +0530

    add user model

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -3,6 +3,7 @@ import { Request } from 'express';
 import { Response } from 'express';
 import { NextFunction } from 'express';
 import createHttpError from 'http-errors';
+import userModel from './userModel';
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
     console.log(req.body);
@@ -11,6 +12,13 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     //validation
     if(!name || !email || !password) {
         const error = createHttpError(400, "All fields are required")
+        return next(error)
+    }
+
+    //Database call
+    const user = await userModel.findOne({ email });
+    if(user) {
+        const error = createHttpError(400, "User already exist")
         return next(error)
     }
     //process
