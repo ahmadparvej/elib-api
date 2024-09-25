0f9f32a5be052ad53ec0a6cd227a1ac4627b1a17
ahmadparvej <ahmadparvej58@gmail.com>
2024-09-25 08:16:30 +0530

    add create user validation

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -2,8 +2,20 @@
 import { Request } from 'express';
 import { Response } from 'express';
 import { NextFunction } from 'express';
+import createHttpError from 'http-errors';
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.body);
+    
+    const { name, email, password } = req.body;
+    //validation
+    if(!name || !email || !password) {
+        const error = createHttpError(400, "All fields are required")
+        return next(error)
+    }
+    //process
+
+    //response
     res.json({
         message: "user created!"
     })
