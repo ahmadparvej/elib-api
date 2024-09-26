dec8d4002552ff59a94f4efd62eec92ddd1fe8b2
ahmadparvej <ahmadparvej58@gmail.com>
2024-09-26 09:04:02 +0530

    add login user

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -58,4 +58,38 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     }
 }
 
-export { createUser }
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+    const { email, password } = req.body;
+
+    //validation
+    if(!email || !password) {
+        const error = createHttpError(400, "All fields are required")
+        return next(error)
+    }
+
+    //Database call
+    try {
+        const user = await userModel.findOne({ email });
+
+        if(!user) {
+            return next(createHttpError(404, "User not found"));
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch) {
+            return next(createHttpError(400, "Username or password incorrect"));
+        }
+
+        const token = sign({ sub: user._id }, config.JWT_SECRET as string, { expiresIn: "7d" });
+
+        res.status(201).json({
+            accessToken: token
+        })
+    } catch (error) {
+        return next(createHttpError(500, "User not found"));
+    }
+
+}
+
+export { createUser, loginUser }
